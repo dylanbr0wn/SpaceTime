1f2f14c8d4dc69478fda03e4860ad3de458f2193
Dylan Brown <dylan.bro97@gmail.com>
2022-04-14 21:46:44 -0700

    set up nexus

---
+++ apps/timesheet2-api/src/index.ts
@@ -0,0 +1,29 @@
+import http from "http";
+
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { GraphQLSchema } from "graphql";
+
+import { Context, context } from "./context";
+import { schema } from "./schema";
+
+async function startApolloServer(context: Context, schema: GraphQLSchema) {
+    const app = express();
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+        context,
+        schema,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+    await server.start();
+    server.applyMiddleware({ app });
+    await new Promise<void>((resolve) =>
+        httpServer.listen({ port: 4000 }, resolve)
+    );
+    console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+    );
+}
+
+startApolloServer(context, schema);
