7a73711993ac65477bf2870e15a3e66086f79e66
Dylan Brown <dylan.bro97@gmail.com>
2022-04-18 11:13:50 -0700

    entry comments

--- apps/timesheet2-api/src/schema/EntryComment.ts
+++ apps/timesheet2-api/src/schema/EntryComment.ts
@@ -1,5 +1,7 @@
-import { objectType } from "nexus";
+import { extendType, intArg, nonNull, objectType, stringArg } from "nexus";
 import * as NexusPrisma from "nexus-prisma";
+
+import { Context } from "../context";
 
 export const EntryComment = objectType({
     name: NexusPrisma.EntryComment.$name,
@@ -10,5 +12,96 @@ export const EntryComment = objectType({
         t.field(NexusPrisma.EntryComment.updatedAt);
         t.field(NexusPrisma.EntryComment.text);
         t.field(NexusPrisma.EntryComment.user);
+    },
+});
+
+export const QueryEntryComment = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.list.nonNull.field("getEntryComments", {
+            type: "EntryComment",
+            args: {
+                timeEntryId: nonNull(intArg()),
+            },
+            resolve: (_parent, args, context: Context) => {
+                return context.prisma.entryComment.findMany({
+                    where: {
+                        timeEntryId: args.id,
+                    },
+                    orderBy: {
+                        createdAt: "desc",
+                    },
+
+                    include: {
+                        user: {
+                            include: {
+                                profile: true,
+                            },
+                        },
+                    },
+                });
+            },
+        });
+        t.nonNull.list.nonNull.field("getEntryCommentsByTimesheet", {
+            type: "EntryComment",
+            args: {
+                timesheetId: nonNull(intArg()),
+            },
+            resolve: (_parent, args, context: Context) => {
+                return context.prisma.entryComment.findMany({
+                    where: {
+                        timeEntry: {
+                            timeEntryRow: {
+                                timesheet: {
+                                    id: args.timesheetId,
+                                },
+                            },
+                        },
+                    },
+                    orderBy: {
+                        createdAt: "desc",
+                    },
+
+                    include: {
+                        user: {
+                            include: {
+                                profile: true,
+                            },
+                        },
+                    },
+                });
+            },
+        });
+    },
+});
+
+export const MutateEntryComment = extendType({
+    type: "Mutation",
+    definition(t) {
+        t.field("createEntryComment", {
+            type: "EntryComment",
+            args: {
+                timeEntryId: nonNull(intArg()),
+                text: nonNull(stringArg()),
+                userId: nonNull(intArg()),
+            },
+            resolve: (_parent, args, context: Context) => {
+                return context.prisma.entryComment.create({
+                    data: {
+                        text: args.text,
+                        timeEntry: {
+                            connect: {
+                                id: args.timeEntryId,
+                            },
+                        },
+                        user: {
+                            connect: {
+                                id: args.userId,
+                            },
+                        },
+                    },
+                });
+            },
+        });
     },
 });
